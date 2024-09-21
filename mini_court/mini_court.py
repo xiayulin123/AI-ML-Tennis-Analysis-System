994fde3d5fc011260863b55aa61c3af53aa395ec
xiayulin123 <xiayulinsky@gmail.com>
2024-09-20 21:53:45 -0400

    added draw background function and some point definition

--- mini_court/mini_court.py
+++ mini_court/mini_court.py
@@ -1,8 +1,12 @@
 import cv2
 import sys
+import numpy as np
 sys.path.append("../")
 import constants
-
+from utils import (
+    convert_meters_to_pixel_distance,
+    convert_pixel_distance_to_meters
+)
 
 class MiniCourt():
     def __init__(self, frame):
@@ -13,6 +17,31 @@ class MiniCourt():
 
         self.set_canvas_background_box(frame)
         self.set_mini_court_position()
+        self.set_court_drawing_keypoints()
+        self.set_court_lines()
+
+
+    def convert_meters_pixels(self, meters):
+        return convert_meters_to_pixel_distance(
+            meters,
+            constants.DOUBLE_LINE_WIDTH,
+            self.court_drawing_width
+        )
+    
+    def set_court_lines(self):
+        self.lines = [
+            (0, 2),
+            (4, 5),
+            (6,7),
+            (1,3),
+            
+            (0,1),
+            (8,9),
+            (10,11),
+            (10,11),
+            (2,3)
+        ]
+
 
     def set_court_drawing_keypoints(self):
         drawing_key_points = [0]*28
@@ -24,8 +53,45 @@ class MiniCourt():
         drawing_key_points[2] , drawing_key_points[3] = int(self.court_end_x), int(self.court_start_y)
 
         # point 2
+        drawing_key_points[4] = int(self.court_start_x)
+        drawing_key_points[5] = int(self.court_end_y) + self.convert_meters_pixels(constants.HALF_COURT_LINE_HEIGHT*2)
         
+        # point 3
+        drawing_key_points[6] = drawing_key_points[0] + self.court_drawing_width
+        drawing_key_points[7] = drawing_key_points[5]
 
+        # point 4
+        drawing_key_points[8] = drawing_key_points[0] +  self.convert_meters_to_pixels(constants.DOUBLE_ALLY_DIFFERENCE)
+        drawing_key_points[9] = drawing_key_points[1] 
+        # #point 5
+        drawing_key_points[10] = drawing_key_points[4] + self.convert_meters_to_pixels(constants.DOUBLE_ALLY_DIFFERENCE)
+        drawing_key_points[11] = drawing_key_points[5] 
+        # #point 6
+        drawing_key_points[12] = drawing_key_points[2] - self.convert_meters_to_pixels(constants.DOUBLE_ALLY_DIFFERENCE)
+        drawing_key_points[13] = drawing_key_points[3] 
+        # #point 7
+        drawing_key_points[14] = drawing_key_points[6] - self.convert_meters_to_pixels(constants.DOUBLE_ALLY_DIFFERENCE)
+        drawing_key_points[15] = drawing_key_points[7] 
+        # #point 8
+        drawing_key_points[16] = drawing_key_points[8] 
+        drawing_key_points[17] = drawing_key_points[9] + self.convert_meters_to_pixels(constants.NO_MANS_LAND_HEIGHT)
+        # # #point 9
+        drawing_key_points[18] = drawing_key_points[16] + self.convert_meters_to_pixels(constants.SINGLE_LINE_WIDTH)
+        drawing_key_points[19] = drawing_key_points[17] 
+        # #point 10
+        drawing_key_points[20] = drawing_key_points[10] 
+        drawing_key_points[21] = drawing_key_points[11] - self.convert_meters_to_pixels(constants.NO_MANS_LAND_HEIGHT)
+        # # #point 11
+        drawing_key_points[22] = drawing_key_points[20] +  self.convert_meters_to_pixels(constants.SINGLE_LINE_WIDTH)
+        drawing_key_points[23] = drawing_key_points[21] 
+        # # #point 12
+        drawing_key_points[24] = int((drawing_key_points[16] + drawing_key_points[18])/2)
+        drawing_key_points[25] = drawing_key_points[17] 
+        # # #point 13
+        drawing_key_points[26] = int((drawing_key_points[20] + drawing_key_points[22])/2)
+        drawing_key_points[27] = drawing_key_points[21] 
+
+        self.drawing_key_points = drawing_key_points
 
 
     def set_mini_court_position(self):
@@ -45,3 +111,17 @@ class MiniCourt():
 
         self.start_y = self.end_y + self.drawing_rectangle_height
         self.start_x = self.end_x + self.drawing_rectangle_width
+
+    def draw_background_rectangle(self, frame):
+        shapes = np.zeros_like(frame, np.unint8)
+
+        # Draw the rectangle
+        cv2.rectangle(shapes, (self.start_x, self.start_y), (self.end_x, self.end_y), (255, 255, 255), -1)
+        out = frame.copy()
+        alpha = 0.5
+        mask = shapes.astype(bool)
+        out[mask] = cv2.addWeighted(frame, alpha, shapes, 1 - alpha, 0)[mask]
+        out = cv2.cvtColor(out, cv2.COLOR_BGR2RGB)
+        return out
+    
+    # def 
